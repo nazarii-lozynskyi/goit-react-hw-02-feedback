cc0de029022677731e6b33ffbfd03074a0bc83b2
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-10-05 21:45:16 +0200

    Update FeedbackOptions.jsx

--- src/components/feedback/feedbackOptions/FeedbackOptions.jsx
+++ src/components/feedback/feedbackOptions/FeedbackOptions.jsx
@@ -1,43 +1,60 @@
 import React from 'react';
 
+import PropTypes from 'prop-types';
+
 import styles from './FeedbackOptions.module.css';
 
-const FeedbackOptions = ({
-  onLeavePositiveFeedback,
-  onLeaveNeutralFeedback,
-  onLeaveNegativeFeedback,
-}) => (
-  <ul className={styles.bntList}>
-    <li className={styles.btnItem}>
-      <button
-        type="button"
-        className={styles.btn}
-        onClick={onLeavePositiveFeedback}
-      >
-        Good
-      </button>
-    </li>
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <ul className={styles.bntList}>
+      {Object.keys(options).map(option => {
+        return (
+          <li className={styles.btnItem} key={option}>
+            <button
+              className={styles.btn}
+              type="button"
+              onClick={onLeaveFeedback}
+            >
+              {option}
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
-    <li className={styles.btnItem}>
-      <button
-        type="button"
-        className={styles.btn}
-        onClick={onLeaveNeutralFeedback}
-      >
-        Neutral
-      </button>
-    </li>
+// const FeedbackOptions = ({
+//   onLeaveFeedback,
+//   options,
+//   // onLeavePositiveFeedback,
+//   // onLeaveNeutralFeedback,
+//   // onLeaveNegativeFeedback,
+// }) => (
+//   <ul className={styles.bntList}>
+//     <li className={styles.btnItem}>
+//       <button type="button" className={styles.btn} onClick={onLeaveFeedback}>
+//         Good
+//       </button>
+//     </li>
 
-    <li className={styles.btnItem}>
-      <button
-        type="button"
-        className={styles.btn}
-        onClick={onLeaveNegativeFeedback}
-      >
-        Bad
-      </button>
-    </li>
-  </ul>
-);
+//     <li className={styles.btnItem}>
+//       <button type="button" className={styles.btn} onClick={onLeaveFeedback}>
+//         Neutral
+//       </button>
+//     </li>
+
+//     <li className={styles.btnItem}>
+//       <button type="button" className={styles.btn} onClick={onLeaveFeedback}>
+//         Bad
+//       </button>
+//     </li>
+//   </ul>
+// );
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.object.isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
 
 export default FeedbackOptions;
